c7bad94224d430bf83f229a6a2fad8f5392a0949
pariwesh06 <pariweshg@gmail.com>
2018-06-25 15:41:15 +0530

    Add files via upload

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent {
+ title = 'app';
+  fname:string='ram';
+  save(){
+    console.log('working');
+  }
+
+}
